b047f834a7db052d6872287c668749d0ffdda7cf
Anton Chervonny <anton75877587@mail.ru>
2023-08-01 21:16:17 +0800

    Исправлены задания 2, 3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,25 +1,24 @@
-const menuItem = document.getElementsByClassName("menu__item");
+const menuLink = document.getElementsByClassName("menu__link");
 const menuActive = document.getElementsByClassName("menu menu_sub menu_active");
 
-for (let i = 0; i < Array.from(menuItem).length; i++) {
-    menuItem[i].onclick = function() {
-        const menuSub = this.querySelector("ul");
-
-        if (!menuSub) {
-            return true;
+function clearActive() {
+    arrayActive = Array.from(menuActive);
+    if (arrayActive.length === 1) {
+        for (let i = 0; i < arrayActive.length; i++) {
+            arrayActive[i].className = "menu menu_sub";
         }
+    }
+}
+
+for (let i = 0; i < Array.from(menuLink).length; i++) {
+    menuLink[i].onclick = function() {
+        const menuSub = this.closest("li").querySelector("ul");
 
         if (menuSub.className === "menu menu_sub") {
-            arrayActive = Array.from(menuActive);
-            if (arrayActive.length === 1) {
-                for (let i = 0; i < arrayActive.length; i++) {
-                    arrayActive[i].className = "menu menu_sub";
-                }
-            }
-
+            clearActive();
             menuSub.className = "menu menu_sub menu_active";
             return false;
-        } else if (menuSub.className === "menu menu_sub menu_active") {
+        } else {
             menuSub.className = "menu menu_sub";
             return false;
         }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,60 +1,64 @@
-const btnPrev = document.getElementsByClassName("slider__arrow slider__arrow_prev");
-const btnNext = document.getElementsByClassName("slider__arrow slider__arrow_next");
+const btnPrev = document.getElementsByClassName("slider__arrow_prev");
+const btnNext = document.getElementsByClassName("slider__arrow_next");
 const images = document.getElementsByClassName("slider__item");
 const sliderDot = document.getElementsByClassName("slider__dot");
 
 let arrayImgs = Array.from(images);
 let arrayDots = Array.from(sliderDot);
-
 arrayDots[0].className = "slider__dot slider__dot_active";
 
+function activateImg(index) {
+    arrayImgs[index].className = "slider__item slider__item_active";
+    arrayDots[index].className = "slider__dot slider__dot_active";
+}
+
+function findAndOffActive() {
+    let index = arrayImgs.findIndex((element) => element.className === "slider__item slider__item_active");
+    arrayImgs[index].className = "slider__item";
+    arrayDots[index].className = "slider__dot"
+    return index;
+}
+
 btnPrev[0].onclick = () => {
-    const activeDot = document.getElementsByClassName("slider__dot slider__dot_active");
+    let index = findAndOffActive();
 
-    if (activeDot.length) {
-        activeDot[0].className = "slider__dot";
-    };
-
-    for (let i = 0; i < arrayImgs.length; i++) {
-        if (arrayImgs[i].className === "slider__item slider__item_active") {
-            if (i === 0) {
-                arrayImgs[i].className = "slider__item";
-                arrayImgs[arrayImgs.length - 1].className = "slider__item slider__item_active";
-                arrayDots[arrayImgs.length - 1].className = "slider__dot slider__dot_active";
-                return;
-            };
-
-            arrayImgs[i].className = "slider__item";
-            arrayImgs[i - 1].className = "slider__item slider__item_active";
-            arrayDots[i - 1].className = "slider__dot slider__dot_active";
-            return;
-        }
+    if (!index) {
+        activateImg(arrayImgs.length - 1);
+    } else {
+        activateImg(index - 1);
     }
 }
 
 btnNext[0].onclick = () => {
-    const activeDot = document.getElementsByClassName("slider__dot slider__dot_active");
+    let index = findAndOffActive();
 
-    if (activeDot.length) {
-        activeDot[0].className = "slider__dot";
-    };
+    if (index + 1 === arrayImgs.length) {
+        activateImg(0);
+    } else {
+        activateImg(index + 1);
+    }
+    
+}
 
-    for (let i = 0; i < arrayImgs.length; i++) {
-        if (arrayImgs[i].className === "slider__item slider__item_active") {
-            if (i === arrayImgs.length - 1) {
-                arrayImgs[i].className = "slider__item";
-                arrayImgs[0].className = "slider__item slider__item_active";
-                arrayDots[0].className ="slider__dot slider__dot_active";
-                return;
-            };
-
-            arrayImgs[i].className = "slider__item";
-            arrayImgs[i + 1].className = "slider__item slider__item_active";
-            arrayDots[i + 1].className = "slider__dot slider__dot_active";
-            return;
-        }
+for (let i = 0; i < arrayDots.length; i++) {
+    sliderDot[i].onclick = () => {
+        findAndOffActive();
+        activateImg(i);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 for (let i = 0; i < arrayDots.length; i++) {
     sliderDot[i].onclick = () => {
